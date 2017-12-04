bfdf67ee094533f064e68afc4632be4177c2dc3f
keerthi <keerthi@tristar-enterprises.com>
2017-12-04 16:00:40 +0400

    Employee Timesheet Upload V2

--- cvs/central_ventilation_systems/doctype/cvs_employee_attendance_and_overtime/cvs_employee_attendance_and_overtime.js
+++ cvs/central_ventilation_systems/doctype/cvs_employee_attendance_and_overtime/cvs_employee_attendance_and_overtime.js
@@ -2,30 +2,39 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('CVS Employee Attendance and Overtime', {
+
 	setup: function(frm) {
-		//console.log(cur_frm.get_docfield("attendances"));
 		frm.get_docfield("attendances").allow_bulk_edit = 1;
-		
+		frm.set_df_property("attendance_date", "read_only", frm.doc.__islocal ? 0 : 1);
 	},
+
+	attendance_date: function(frm, cdt, cdn) {
+		var child = locals[cdt][cdn];
+		console.log(child.attendances);
+		if (child.attendances){
+			for (var i =0; i < cur_frm.doc.attendances.length; i++){
+				cur_frm.doc.attendances[i].attendance_date = cur_frm.doc.attendance_date
+			}
+			cur_frm.refresh_field('attendances')
+		}
+		else{
+			console.log("New")
+		}
+	},
+
 	refresh: function(frm) {
-
+		frm.set_df_property("attendance_date", "read_only", frm.doc.__islocal ? 0 : 1);
 	}
-
-
 });
 
-
 frappe.ui.form.on("CVS Employee Attendance", {
-	
+
 	out_time: function(frm, cdt, cdn) {
 	var child = locals[cdt][cdn];
-	console.log("Test");
 	var startDate = "01/01/2018 " + child.in_time;
 	var endDate = "01/01/2018 " + child.out_time;
 	var normal_ot = document.getElementById('normal_ot');
 	var special_ot = document.getElementById('special_ot');
-	console.log(startDate);
-	console.log(child.holiday);
 	if (child.holiday == 1){
 		if (child.eligible_for_overtime == 1){
 			frappe.model.set_value(cdt, cdn, "special_ot", moment(endDate).diff(moment(startDate),"seconds") / 3600);
@@ -37,9 +46,7 @@ frappe.ui.form.on("CVS Employee Attendance", {
 	else{
 		frappe.model.set_value(cdt, cdn, "total_hours", moment(endDate).diff(moment(startDate),"seconds") / 3600);
 	}
-	
 	},
-
 
 	total_hours: function(frm, cdt, cdn) {
 	var child = locals[cdt][cdn];
@@ -48,33 +55,22 @@ frappe.ui.form.on("CVS Employee Attendance", {
 		if (child.eligible_for_overtime == 1) {
 			console.log("Employee Eligible For Overtime");
 			frappe.model.set_value(cdt, cdn, "normal_ot", child.total_hours - 8.00 );
-		/*	query: "cvs.central_ventilation_systems.doctype.cvs_employee_attendance_and_overtime.get_attendance_settings"
-			console.log(query);*/
-
 		}
 		else {
 			console.log("Employee Ineligible For Overtime");
 		}
-
 	}
 	else {
 		console.log("Normal");
 	}
 	},
 
-
 	in_time: function(frm, cdt, cdn) {
 	var child = locals[cdt][cdn];
 	if (child.in_time){
-		console.log(in_time);
 		var in_time = "01/01/2018 " + child.in_time;
 		var office_in_time = "01/01/2018 " + child.default_in;
-		console.log("in_time",in_time);
-		console.log("office_in_time",office_in_time);
 		frappe.model.set_value(cdt, cdn, "latency", moment(office_in_time).diff(moment(in_time),"seconds") / 3600);
-
 	}
-
 	},
-	
 });
--- cvs/central_ventilation_systems/doctype/cvs_employee_attendance_and_overtime/cvs_employee_attendance_and_overtime.py
+++ cvs/central_ventilation_systems/doctype/cvs_employee_attendance_and_overtime/cvs_employee_attendance_and_overtime.py
@@ -8,19 +8,26 @@ from frappe.model.document import Document
 
 class CVSEmployeeAttendanceandOvertime(Document):
 
-
 	def get_employees_detail(self):
-		departments = self.departments
 		# Clear Child Table
 		self.set('attendances', {})
-        # Iterating Data Based On Departments
+		# Iterating Data Based On Departments
+		post_date = self.get('attendance_date');
+		in_time = frappe.db.sql("Select value from tabSingles where doctype = 'CVS Employee Attendance Settings' and field = 'in_time';")
+		out_time = frappe.db.sql("Select value from tabSingles where doctype = 'CVS Employee Attendance Settings' and field = 'out_time';")
+		breaktime = frappe.db.sql("Select value from tabSingles where doctype = 'CVS Employee Attendance Settings' and field = 'break_time';")
+		holiday = frappe.db.sql("Select name from tabHoliday where holiday_date	 = %s;",(post_date,), as_dict = 1)
 		for d in self.get('departments'):
-			post_date = self.get('attendance_date');
-			employees = frappe.db.sql("Select name, department, employee_name, eligible_for_overtime from tabEmployee where department  = %s", (d.department,), as_dict = 1)
-			in_time = frappe.db.sql("Select value from tabSingles where doctype = 'CVS Employee Attendance Settings' and field = 'in_time';")
-			out_time = frappe.db.sql("Select value from tabSingles where doctype = 'CVS Employee Attendance Settings' and field = 'out_time';")
-			breaktime = frappe.db.sql("Select value from tabSingles where doctype = 'CVS Employee Attendance Settings' and field = 'break_time';")
-			holiday = frappe.db.sql("Select name from tabHoliday where holiday_date	 = %s;",(post_date,), as_dict = 1)
+			# employees = frappe.db.sql("Select name, department, employee_name, eligible_for_overtime from tabEmployee where department  = %s", (d.department,), as_dict = 1)
+			employees = frappe.db.sql(""" Select emp.name, emp.department, 
+										emp.employee_name, emp.eligible_for_overtime
+										FROM tabEmployee as emp WHERE department  = %s AND
+										NOT EXISTS (SELECT 1 from `tabLeave Application` as la
+										WHERE la.employee = emp.name
+										AND la.status = 'Approved'
+										AND %s BETWEEN la.from_date 
+										AND la.to_date);""", (d.department,post_date,), as_dict = 1)
+			# leave = frappe.db.sql("Select employee from `tabLeave Allocation` where from_date = %s or to_date = %s;",(post_date,post_date,), as_dict = 1)
 			# Parsing the Data To Child Table
 			for employee in employees:
 				attendance = self.append('attendances', {})
@@ -44,3 +51,32 @@ class CVSEmployeeAttendanceandOvertime(Document):
 					attendance.total_hours = 8
 				attendance.attendance_date = post_date
 				attendance.latency = 0
+
+	def on_submit(self):
+		attendences = self.get('attendances')
+		for attendence in attendences:
+			if attendence.total_hours == 0:
+				s = frappe.new_doc("Leave Application")
+				s.from_date = attendence.attendance_date
+				s.to_date = attendence.attendance_date
+				s.employee = attendence.employee
+				s.leave_type = "Privilege Leave"
+				s.insert()
+				s.save()					
+				# s.submit()
+
+				return s
+
+		return True
+
+
+
+
+
+
+
+
+
+
+
+
