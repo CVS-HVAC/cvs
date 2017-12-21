fb6e65c9ce2eb909effa5994831fed18ba38300e
keerthi <keerthi@tristar-enterprises.com>
2017-12-21 19:48:39 +0400

    Employee Overtime 3.0

--- cvs/central_ventilation_systems/doctype/cvs_employee_attendance_and_overtime/cvs_employee_attendance_and_overtime.js
+++ cvs/central_ventilation_systems/doctype/cvs_employee_attendance_and_overtime/cvs_employee_attendance_and_overtime.js
@@ -6,11 +6,32 @@ frappe.ui.form.on('CVS Employee Attendance and Overtime', {
 	setup: function(frm) {
 		frm.get_docfield("attendances").allow_bulk_edit = 1;
 		frm.set_df_property("attendance_date", "read_only", frm.doc.__islocal ? 0 : 1);
+		// frm.setup_queries();
+	},
+
+
+	onload: function(frm) {
+		frm.set_query("department", "departments", function() {
+				 return{
+					filters:{ 
+								'is_attendance_required' : ["=", 1]
+							}
+						}
+			
+		});
+
+		frm.set_query("employee", "attendances", function() {
+				 return{
+					filters:{ 
+								'department' : ["!=", "Management"]
+							}
+						}
+			
+		});
 	},
 
 	attendance_date: function(frm, cdt, cdn) {
 		var child = locals[cdt][cdn];
-		console.log(child.attendances);
 		if (child.attendances){
 			for (var i =0; i < cur_frm.doc.attendances.length; i++){
 				cur_frm.doc.attendances[i].attendance_date = cur_frm.doc.attendance_date
@@ -24,53 +45,171 @@ frappe.ui.form.on('CVS Employee Attendance and Overtime', {
 
 	refresh: function(frm) {
 		frm.set_df_property("attendance_date", "read_only", frm.doc.__islocal ? 0 : 1);
+	},
+
+	setup_queries: function() {
+		var me = this;
+		this.frm.set_query("department", "departments", function() {
+			if(me.frm.doc.department) {
+				 return{
+					filters:{ 
+								'name' : ["!=", "Management"]
+							
+							}
+						}
+			} 
+		});
 	}
+
+
 });
+
 
 frappe.ui.form.on("CVS Employee Attendance", {
 
 	out_time: function(frm, cdt, cdn) {
 	var child = locals[cdt][cdn];
+	if (child.out_time != 0){
+		if (parseFloat(child.in_time) >= parseFloat(child.out_time)) {
+			frappe.msgprint(__("InTime  Should Be Lesser Than OutTime."));
+		}
 	var startDate = "01/01/2018 " + child.in_time;
 	var endDate = "01/01/2018 " + child.out_time;
 	var normal_ot = document.getElementById('normal_ot');
 	var special_ot = document.getElementById('special_ot');
-	if (child.holiday == 1){
-		if (child.eligible_for_overtime == 1){
-			frappe.model.set_value(cdt, cdn, "special_ot", moment(endDate).diff(moment(startDate),"seconds") / 3600);
-		}
-		else{
-			frappe.model.set_value(cdt, cdn, "total_hours", moment(endDate).diff(moment(startDate),"seconds") / 3600);
-		}
-	}
-	else{
-		frappe.model.set_value(cdt, cdn, "total_hours", moment(endDate).diff(moment(startDate),"seconds") / 3600);
+
+	// frappe.model.set_value(cdt, cdn, "total_hours", 0);
+	frappe.model.set_value(cdt, cdn, "special_ot", 0);
+	// frappe.model.set_value(cdt, cdn, "paid_hours", 0);
+	frappe.model.set_value(cdt, cdn, "normal_ot", 0);
+	frappe.call({
+			method: "cvs.central_ventilation_systems.doctype.cvs_employee_attendance_and_overtime.cvs_employee_attendance_and_overtime.get_outtime",
+			args: {
+				"intime" : child.in_time,
+				"outtime" : child.out_time
+			},
+			callback: function(r) {
+				var out = frappe.model.sync(r.message);
+				frappe.model.set_value(cdt, cdn, "out_time",out[0]['time_out'] );
+				if (child.holiday == 1){
+					if (child.eligible_for_overtime == 1){
+						frappe.model.set_value(cdt, cdn, "special_ot", out[0]['total']);
+					}
+					else{
+						frappe.model.set_value(cdt, cdn, "total_hours", out[0]['total']);
+					}
+				}
+				else{
+					frappe.model.set_value(cdt, cdn, "total_hours", out[0]['total']);
+				}
+			}
+		});
 	}
 	},
 
 	total_hours: function(frm, cdt, cdn) {
 	var child = locals[cdt][cdn];
-	if (child.total_hours > 8.00){
+	if (child.total_hours >= 24) {
+			frappe.msgprint(__("Total Hours Should Not Exceed Above 23."));
+		}
+
+	total_hours = parseFloat(child.total_hours);
+	paid_hours = parseFloat(child.paid_hours);
+	break_time = parseFloat(child.breaktime);
+	default_paid_hrs = parseFloat(child.default_paid_hrs);
+
+		var Total_hour = total_hours,
+		    Paid_hour = paid_hours,
+		    Break_hour = break_time;
+		    Default_hour = default_paid_hrs
+
+		var conversionInMinutes = hour => Math.floor(hour) * 60 + (hour - (Math.floor(hour))) * 100;
+		var conversionInHours = min => Math.floor( min/60 ) + min % 60 / 100;
+		var Remaining_hour = conversionInMinutes(Total_hour) - (conversionInMinutes(Paid_hour) + conversionInMinutes(Break_hour));
+		tot = conversionInHours(Remaining_hour).toFixed(2);
+
+		var overtime = conversionInMinutes(Total_hour) - conversionInMinutes(Paid_hour) - conversionInMinutes(Break_hour);
+		normal_ot = conversionInHours(overtime).toFixed(2);
+
+		var paid = conversionInMinutes(Default_hour) - conversionInMinutes(Total_hour) - conversionInMinutes(Break_hour);
+		paid_hrs = conversionInHours(paid).toFixed(2);
+
+
+		var diff = conversionInMinutes(Total_hour) - conversionInMinutes(Break_hour);
+		diff_time = conversionInHours(diff).toFixed(2);
+
+	//tot = convertToMs(default_paid_hrs) - (convertToMs(total_hours) + convertToMs(break_time));
+	
+	if (total_hours > default_paid_hrs){
 		console.log("Overtime");
 		if (child.eligible_for_overtime == 1) {
 			console.log("Employee Eligible For Overtime");
-			frappe.model.set_value(cdt, cdn, "normal_ot", child.total_hours - 8.00 );
+			// normal_ot = total_hours - default_paid_hrs - break_time
+			if (normal_ot > 0){
+				frappe.model.set_value(cdt, cdn, "normal_ot", normal_ot );
+			}
+			// paid = default_paid_hrs - (total_hours - break_time);
+			if (paid_hrs > 0){
+				frappe.model.set_value(cdt, cdn, "paid_hours",  paid_hrs);
+			}
+			else{
+				frappe.model.set_value(cdt, cdn, "paid_hours", default_paid_hrs );
+			}
 		}
 		else {
 			console.log("Employee Ineligible For Overtime");
 		}
 	}
 	else {
-		console.log("Normal");
+		frappe.model.set_value(cdt, cdn, "paid_hours", diff_time);
+		frappe.model.set_value(cdt, cdn, "normal_ot", 0);
+
 	}
 	},
 
 	in_time: function(frm, cdt, cdn) {
 	var child = locals[cdt][cdn];
-	if (child.in_time){
+	if (child.in_time != 0){
+		if (parseFloat(child.in_time) >= parseFloat(child.out_time)) {
+			frappe.msgprint(__("InTime  Should Be Lesser Than OutTime."));
+		}
 		var in_time = "01/01/2018 " + child.in_time;
 		var office_in_time = "01/01/2018 " + child.default_in;
-		frappe.model.set_value(cdt, cdn, "latency", moment(office_in_time).diff(moment(in_time),"seconds") / 3600);
+		frappe.call({
+			method: "cvs.central_ventilation_systems.doctype.cvs_employee_attendance_and_overtime.cvs_employee_attendance_and_overtime.get_intime",
+			args: {
+				"intime" : child.in_time,
+				"default_in" : child.default_in
+			},
+			callback: function(r) {
+				var inn = frappe.model.sync(r.message);
+				frappe.model.set_value(cdt, cdn, "in_time",inn[0]['time_in'] );
+				frappe.model.set_value(cdt, cdn, "latency",inn[0]['latency']);
+			}
+		});
 	}
+	},
+
+	employee: function(frm, cdt, cdn) {
+	var child = locals[cdt][cdn];
+	attendance_date = child.attendance_date
+	frappe.call({
+			method: "cvs.central_ventilation_systems.doctype.cvs_employee_attendance_and_overtime.cvs_employee_attendance_and_overtime.get_emp_depart",
+			args: {
+				"employee" : child.employee
+			},
+			callback: function(r) {
+				var att = frappe.model.sync(r.message);
+				frappe.model.set_value(cdt, cdn, "in_time",att[0]['in_time'] );
+				frappe.model.set_value(cdt, cdn, "out_time", att[0]['out_time']);
+				frappe.model.set_value(cdt, cdn, "paid_hours", att[0]['paid_hrs']);
+				frappe.model.set_value(cdt, cdn, "breaktime", att[0]['break_time']);
+				frappe.model.set_value(cdt, cdn, "eligible_for_overtime", att[0]['eligible_for_overtime']);
+				frappe.model.set_value(cdt, cdn, "employee_name", att[0]['employee_name']);
+				frappe.model.set_value(cdt, cdn, "department", att[0]['department']);
+				frappe.model.set_value(cdt, cdn, "default_paid_hrs", att[0]['paid_hrs']);
+
+			}
+		});
 	},
 });
--- cvs/central_ventilation_systems/doctype/cvs_employee_attendance_and_overtime/cvs_employee_attendance_and_overtime.py
+++ cvs/central_ventilation_systems/doctype/cvs_employee_attendance_and_overtime/cvs_employee_attendance_and_overtime.py
@@ -5,8 +5,57 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
-
+from frappe import _, msgprint, throw
+import math
 class CVSEmployeeAttendanceandOvertime(Document):
+
+	def autoname(self):
+		attendance_date = self.attendance_date
+		from datetime import datetime
+		self.employee_date = datetime.strptime(attendance_date, '%Y-%m-%d').strftime('%d-%m-%Y')
+		return True
+
+	def conversionInHours(self, hrs):
+		hours = math.floor( hrs/60 ) + hrs % 60 / 100;
+		return hours
+
+	def conversionInMinutes(self, mins):
+		minutes = math.floor(float(mins)) * 60 + (float(mins) - (math.floor(float(mins)))) * 100
+		return minutes
+
+	def get_totaltime(self, in_time, out_time, default_in):
+
+		actual_in = in_time
+		data = {}
+		inn = "01-01-2018 " + in_time;
+		out = "01-01-2018 " + out_time;
+		from datetime import datetime
+		time_in = datetime.strptime(inn, '%d-%m-%Y %H:%M:%S')
+		time_out = datetime.strptime(out, '%d-%m-%Y %H:%M:%S')
+		import datetime
+		intime =  datetime.datetime(time_in.year, time_in.month, time_in.day, time_in.hour,15*(time_in.minute // 15))
+		in_time = intime.strftime('%H:%M:%S')
+		outtime =  datetime.datetime(time_out.year, time_out.month, time_out.day, time_out.hour,15*(time_out.minute // 15))
+		out_time = outtime.strftime('%H:%M:%S')
+		from datetime import datetime
+		FMT = '%H:%M:%S'
+		diff = datetime.strptime(out_time, FMT) - datetime.strptime(in_time, FMT)
+		hours, remainder = divmod(diff.seconds, 3600)
+		minutes, seconds = divmod(remainder, 60)
+		total =  str(hours) +"."+ str(minutes)
+
+		hours, remainder = divmod(default_in.seconds, 3600)
+		minutes, seconds = divmod(remainder, 60)
+		default_in =  str(hours) +":"+ str(minutes) +":00"
+
+		tdelta = datetime.strptime(in_time, FMT) - datetime.strptime(default_in, FMT)
+		hours, remainder = divmod(tdelta.seconds, 3600)
+		minutes, seconds = divmod(remainder, 60)
+		latency =  str(hours) +"."+ str(minutes)
+		data.update({'latency':latency})
+		data.update({'time_out':out_time})
+		data.update({'total':total})
+		return data
 
 	def get_employees_detail(self):
 		# Clear Child Table
@@ -18,16 +67,24 @@ class CVSEmployeeAttendanceandOvertime(Document):
 		breaktime = frappe.db.sql("Select value from tabSingles where doctype = 'CVS Employee Attendance Settings' and field = 'break_time';")
 		holiday = frappe.db.sql("Select name from tabHoliday where holiday_date	 = %s;",(post_date,), as_dict = 1)
 		for d in self.get('departments'):
-			# employees = frappe.db.sql("Select name, department, employee_name, eligible_for_overtime from tabEmployee where department  = %s", (d.department,), as_dict = 1)
-			employees = frappe.db.sql(""" Select emp.name, emp.department, 
-										emp.employee_name, emp.eligible_for_overtime
-										FROM tabEmployee as emp WHERE department  = %s AND
-										NOT EXISTS (SELECT 1 from `tabLeave Application` as la
-										WHERE la.employee = emp.name
-										AND la.status = 'Approved'
-										AND %s BETWEEN la.from_date 
-										AND la.to_date);""", (d.department,post_date,), as_dict = 1)
-			# leave = frappe.db.sql("Select employee from `tabLeave Allocation` where from_date = %s or to_date = %s;",(post_date,post_date,), as_dict = 1)
+			employees = frappe.db.sql(""" Select  	emp.name, 
+													emp.department, emp.employee_name, 
+													emp.eligible_for_overtime, 
+													dep.in_time, dep.out_time,
+													dep.break_time, dep.paid_hrs
+													FROM tabEmployee as emp 
+													JOIN tabDepartment as dep
+													WHERE emp.department  = %s AND
+													dep.name  = %s AND
+													NOT EXISTS (SELECT 1 from `tabLeave Application` as la
+													WHERE la.employee = emp.name
+													AND la.status = 'Approved'
+													AND %s BETWEEN la.from_date 
+													AND la.to_date) AND
+													NOT EXISTS (Select 1 from `tabCVS Employee Attendance` as ea
+													WHERE ea.employee = emp.name 
+													AND ea.attendance_date = %s);""", (d.department,d.department,post_date,post_date,), as_dict = 1)
+
 			# Parsing the Data To Child Table
 			for employee in employees:
 				attendance = self.append('attendances', {})
@@ -35,48 +92,192 @@ class CVSEmployeeAttendanceandOvertime(Document):
 				attendance.department = employee.department
 				attendance.employee_name = employee.employee_name
 				attendance.eligible_for_overtime = employee.eligible_for_overtime
-				attendance.in_time = in_time[0][0]
-				attendance.out_time = out_time[0][0]
-				attendance.default_in = in_time[0][0]
-				attendance.default_out = out_time[0][0]
-				attendance.breaktime = breaktime[0][0]
+				attendance.in_time = employee.in_time #in_time[0][0]
+				attendance.out_time = employee.out_time #out_time[0][0]
+				attendance.default_in = employee.in_time #in_time[0][0]
+				attendance.default_out = employee.out_time #out_time[0][0]
+				attendance.breaktime = employee.break_time #breaktime[0][0]
+				attendance.paid_hours = employee.paid_hrs
+				attendance.default_paid_hrs = employee.paid_hrs
+				date_format = "%m-%d-%Y %H:%M:%S"
+				from datetime import datetime
+				inn = "1-01-2018 " + str(employee.in_time)
+				out = "1-01-2018 " + str(employee.out_time)
+				time1  = datetime.strptime(inn, date_format)
+				time2  = datetime.strptime(out, date_format)
+				diff = time2 - time1
+				diff_btw_times = (diff.seconds) / 3600
+				attendance.total_hours = diff_btw_times
 				if holiday:
 					attendance.holiday = 1
 					if employee.eligible_for_overtime:
-						attendance.special_ot = 8
+						attendance.special_ot = diff_btw_times
 					else:
-						attendance.total_hours = 8
+						attendance.total_hours = diff_btw_times
 				else:
 					attendance.holiday = 0
-					attendance.total_hours = 8
+					attendance.total_hours = diff_btw_times
 				attendance.attendance_date = post_date
 				attendance.latency = 0
 
+	def validate(self):
+		self.set_missing_values();
+
+	def set_missing_values(self, for_validate=False):
+		post_date = self.get('attendance_date');
+		if self.attendances:
+			for attendance in self.attendances:
+				if len(attendance.out_time) == 1:
+					attendance.paid_hours = 0
+					attendance.normal_ot = 0
+					attendance.special_ot = 0
+				else:
+					exist = frappe.db.sql(""" Select 1 from `tabCVS Employee Attendance` 
+												where employee = %s
+												and attendance_date = %s""", (attendance.employee,self.attendance_date,), as_dict=1)
+					if exist:
+						frappe.throw(_("Employee "+attendance.employee+" Already Created For The Attendance Date."))
+
+					holiday = frappe.db.sql("Select name from tabHoliday where holiday_date	 = %s;",(self.attendance_date,), as_dict = 1)
+					res = frappe.db.sql("""
+										Select  e.department, e.employee_name, 
+										e.eligible_for_overtime, d.in_time, d.out_time,
+										d.break_time, d.paid_hrs from tabDepartment d 
+										join tabEmployee e on (e.department = d.name) 
+										where e.name = %s
+										""", (attendance.employee), as_dict=1)
+
+					if not res:
+						throw(_('Invalid Employee Data'))
+						return {}
+					else:
+						res = res[0]
+					if not attendance.attendance_date:
+						attendance.attendance_date = post_date
+					if not attendance.breaktime:
+						attendance.breaktime = res['break_time']
+					if not attendance.eligible_for_overtime:
+						attendance.eligible_for_overtime = res['eligible_for_overtime']
+					if not attendance.employee_name:
+						attendance.employee_name = res['employee_name']
+					if not attendance.department:
+						attendance.department = res['department']
+					if not attendance.default_paid_hrs:
+						attendance.default_paid_hrs = res['paid_hrs']
+					if not attendance.total_hours:
+						tot = self.get_totaltime(attendance.in_time,attendance.out_time,res['in_time'])
+						attendance.total_hours = tot['total']
+						attendance.latency = tot['latency']
+						if not attendance.paid_hours:
+							# attendance.paid_hours = res['paid_hrs']
+							total = self.conversionInMinutes(tot['total']) - self.conversionInMinutes(res['break_time'])
+							totalhrs = self.conversionInHours(total)
+
+							if float(totalhrs) > float(res['paid_hrs']):
+								attendance.paid_hours = res['paid_hrs']
+							else:
+								attendance.paid_hours = totalhrs
+						extra = self.conversionInMinutes(tot['total']) - self.conversionInMinutes(res['paid_hrs']) - self.conversionInMinutes(res['break_time'])
+						extra_hrs = self.conversionInHours(extra)
+						holiday = frappe.db.sql("Select name from tabHoliday where holiday_date	 = %s;",(self.attendance_date,), as_dict = 1)
+						if float(extra_hrs) > 0:
+							if res['eligible_for_overtime'] == 1:
+								if holiday:
+									attendance.holiday = 1
+									attendance.special_ot = extra_hrs
+								else:	
+									attendance.normal_ot = extra_hrs
+					if not attendance.holiday:
+						if res['eligible_for_overtime'] == 1:
+							if holiday:
+								tot = self.get_totaltime(attendance.in_time,attendance.out_time,res['in_time'])
+								# extra = self.conversionInMinutes(tot['total']) - self.conversionInMinutes(res['paid_hrs']) - self.conversionInMinutes(res['break_time'])
+								# extra_hrs = self.conversionInHours(extra)
+								attendance.holiday = 1
+								attendance.special_ot = tot['total']
+								attendance.normal_ot = 0
 	def on_submit(self):
 		attendences = self.get('attendances')
-		for attendence in attendences:
-			if attendence.total_hours == 0:
-				s = frappe.new_doc("Leave Application")
-				s.from_date = attendence.attendance_date
-				s.to_date = attendence.attendance_date
-				s.employee = attendence.employee
-				s.leave_type = "Privilege Leave"
-				s.insert()
-				s.save()					
-				# s.submit()
+		departments = self.get('departments')
+		leave = frappe.db.sql("""Select value from tabSingles where doctype = 'CVS Employee Attendance Settings' and field = 'leave';""")
+		if departments:
+			if attendences:
+				for attendence in attendences:
+					if attendence.total_hours == 0:
+						s = frappe.new_doc("Leave Application")
+						s.from_date = attendence.attendance_date
+						s.to_date = attendence.attendance_date
+						s.employee = attendence.employee
+						s.leave_type = leave[0][0]
+						s.insert()
+						s.save()					
+						# s.submit()
 
-				return s
+						return s
+			else:
+				frappe.throw(_("Please Choose The Employee"))
+
+		else:
+			frappe.throw(_("Please Choose The Department"))
 
 		return True
 
+@frappe.whitelist()
+def get_emp_depart(employee, depart = None):
+	res = frappe.db.sql("""
+		Select  e.department, e.employee_name, 
+				e.eligible_for_overtime, d.in_time, d.out_time,
+				d.break_time, d.paid_hrs from tabDepartment d 
+				join tabEmployee e on (e.department = d.name) 
+				where e.name = %s
+	""", (employee), as_dict=1)
 
+	if not res:
+		return {}
 
+	res = res[0]
+	return res
 
+@frappe.whitelist()
+def get_intime(intime, default_in):
+	actual_in = intime
+	data = {}
+	inn = "01-01-2018 " + intime;
+	from datetime import datetime
+	time_in = datetime.strptime(inn, '%d-%m-%Y %H:%M:%S')
+	import datetime
+	intime =  datetime.datetime(time_in.year, time_in.month, time_in.day, time_in.hour,15*(time_in.minute // 15))
+	in_time = intime.strftime('%H:%M:%S')
+	from datetime import datetime
+	FMT = '%H:%M:%S'
+	tdelta = datetime.strptime(actual_in, FMT) - datetime.strptime(default_in, FMT)
+	hours, remainder = divmod(tdelta.seconds, 3600)
+	minutes, seconds = divmod(remainder, 60)
+	latency =  str(hours) +"."+ str(minutes)
+	data.update({'time_in':actual_in})
+	data.update({'latency':latency})
+	return data
 
-
-
-
-
-
-
-
+@frappe.whitelist()
+def get_outtime(intime, outtime):
+	actual_out = outtime
+	data = {}
+	inn = "01-01-2018 " + intime;
+	out = "01-01-2018 " + outtime;
+	from datetime import datetime
+	time_in = datetime.strptime(inn, '%d-%m-%Y %H:%M:%S')
+	time_out = datetime.strptime(out, '%d-%m-%Y %H:%M:%S')
+	import datetime
+	intime =  datetime.datetime(time_in.year, time_in.month, time_in.day, time_in.hour,15*(time_in.minute // 15))
+	in_time = intime.strftime('%H:%M:%S')
+	outtime =  datetime.datetime(time_out.year, time_out.month, time_out.day, time_out.hour,15*(time_out.minute // 15))
+	out_time = outtime.strftime('%H:%M:%S')
+	from datetime import datetime
+	FMT = '%H:%M:%S'
+	diff = datetime.strptime(out_time, FMT) - datetime.strptime(in_time, FMT)
+	hours, remainder = divmod(diff.seconds, 3600)
+	minutes, seconds = divmod(remainder, 60)
+	total =  str(hours) +"."+ str(minutes)
+	data.update({'time_out':actual_out})
+	data.update({'total':total})
+	return data
